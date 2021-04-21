470d2c737178c3c2e55d5198f976daf9e5fc8590
Leozillo73 <daniel.kashtanov@gmail.com>
2021-04-21 19:50:29 +0400

    Добавил поворот камеры и сделал стены непроницаемыми

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+
+public class DoorControl : MonoBehaviour
+{
+
+	public float distance = 5; // в приделах этой дистанции дверь будет доступна
+	public string doorTag = "Door"; // тег двери
+	public KeyCode key = KeyCode.F; // клавиша управления
+	private Camera cam;
+
+	void Awake()
+	{
+		cam = GetComponent<Camera>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(key))
+		{
+			RaycastHit hit;
+			Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+			if (Physics.Raycast(ray, out hit, distance))
+			{
+				if (hit.collider.tag == doorTag)
+				{
+					hit.transform.GetComponent<Door>().enabled = true;
+					hit.transform.GetComponent<Door>().Invert(transform);
+				}
+			}
+		}
+	}
+}
--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    public GameObject player;
-    public float speed = 2f;
-    private Vector3 _direction = Vector3.zero;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject player;
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float _MouseSensetive;
+    
+    Vector3 _direction = Vector3.zero;
+    float _angle;
     void Start()
     {
-        player=(GameObject)this.gameObject;
+        
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    _direction.z = 1;
-        //}
         _direction.x = Input.GetAxis("Horizontal");
         _direction.z = Input.GetAxis("Vertical");
-        
+        _angle = Input.GetAxis("Mouse X");
+        //_angle.y = Input.GetAxis("Mouse Y");
     }
     private void FixedUpdate()
     {
-        var _speed = _direction.normalized * Time.fixedDeltaTime * speed;
+        Move();
+        Camerarotator();
+    }
+    private void Camerarotator()
+    {
+        player.transform.Rotate(new Vector3(0f, _angle*_MouseSensetive*Time.fixedDeltaTime, 0f));
+    }
+    private void Move()
+    {
+        var _speed = _direction * Time.fixedDeltaTime * speed;
         player.transform.Translate(_speed);
     }
 }
