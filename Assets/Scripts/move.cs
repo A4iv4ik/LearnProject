a16eca9f66693f96fa25b702e17d57a3b81f6eab
Leozillo73 <daniel.kashtanov@gmail.com>
2021-04-25 19:32:07 +0400

    префаб канваса

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class move : MonoBehaviour
 {
@@ -8,24 +9,21 @@ public class move : MonoBehaviour
     [SerializeField] private float speed = 2f;
     [SerializeField] private float _MouseSensetive;
     [SerializeField] private Transform _camera;
- 
-
-    
     Vector3 _direction = Vector3.zero;
     float _angle;
 
     void Update()
     {
-      
+        
         _direction.x = Input.GetAxis("Horizontal");
         _direction.z = Input.GetAxis("Vertical");
         _angle = Input.GetAxis("Mouse X");
-    
         Camerarotator();
     }
     private void FixedUpdate()
     {
         Move();
+        
     }
     private void Camerarotator()
     {
@@ -36,4 +34,6 @@ public class move : MonoBehaviour
         var _speed = _direction * Time.fixedDeltaTime * speed;
         player.transform.Translate(_speed);
     }
+
 }
+
