4bfcd6ded591ff4a23ccfbe0d7dc2ce51a74ffb6
Leozillo73 <daniel.kashtanov@gmail.com>
2021-05-10 18:47:18 +0400

    Состыкавал анимации дорагона

--- Assets/Scripts/Doragon.cs
+++ Assets/Scripts/Doragon.cs
@@ -8,43 +8,50 @@ public class Doragon : MonoBehaviour
     private Transform _player;
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private Animator _animator;
-    private bool at1;
-    private bool at2;
-    private bool at3;
+    private bool at1=true;
+    private bool at2=true;
+    private bool at3=true;
     private bool sleep=true;
     private bool walk=false;
+    private float health;
+    private float distance;
     private void Awake()
     {
         _player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
     private void Update()
     {
-        var distance = Vector3.Distance(transform.position, _player.position);
-        if (distance <8f&&at1)
+         distance = Vector3.Distance(transform.position, _player.position);
+        if (distance>20)
+            _animator.SetBool("Walk",true);
+        else
+            _animator.SetBool("Walk", false);
+
+        if (distance <10f&&at1)
         {
             _animator.SetTrigger("FirstAttack");
             at1 = false;
             at2 = true;
+            
         }
-        if (distance < 12f && distance>8f && at2)
+        if (distance < 16f && distance>10f && at2)
         {
             _animator.SetTrigger("SecondAttack");
+            StartCoroutine(Atack2());
             at2 = false;
             at3 = true;
             at1 = true;
         }
-        if ( distance > 12f && at3)
+        else StopCoroutine(Atack2());
+        if ( distance > 16f && at3)
         {
+            
             _animator.SetTrigger("ThirdAttack");
             at3 = false;
             at2 = true;
             at1 = true;
         }
-    }
-    private void FixedUpdate()
-    {
         Move();
-        
     }
     private void Move()
     {
@@ -57,6 +64,7 @@ public class Doragon : MonoBehaviour
             {
                 if (sleep)
                 {
+                    StartCoroutine(Chill());
                     _animator.SetTrigger("UnSleep");
                     sleep = false;
                     StartCoroutine(ReadyToWalk());
@@ -77,20 +85,27 @@ public class Doragon : MonoBehaviour
     }
     IEnumerator Atack2()
     {
-        while (true)
+        while (distance < 16f && distance > 10f)
         {
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(6f);
+            if (at2==false)
+            {
+            _animator.SetTrigger("SecondAttack");
 
-            new WaitForSeconds(3f);
+            }
+
         }
     }
-    IEnumerator Atack3()
+    IEnumerator Chill()
     {
         while (true)
         {
-            yield return new WaitForSeconds(3f);
-
-            new WaitForSeconds(3f);
+            yield return new WaitForSeconds(30f);
+            _animator.SetTrigger("Chill");
+            walk = false;
+            yield return new WaitForSeconds(5f);
+            _animator.SetTrigger("UnChill");
+            walk = true;
         }
     }
 }
